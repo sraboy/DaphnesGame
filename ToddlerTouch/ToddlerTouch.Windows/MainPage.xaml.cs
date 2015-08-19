ae522c89e9567096020622d701b05eef04ef0bc3
sraboy <steven.lavoiejr@gmail.com>
2015-08-18 20:48:14 -1000

    randomized colors and ellipse size. works up to level 8

--- ToddlerTouch/ToddlerTouch.Windows/MainPage.xaml.cs
+++ ToddlerTouch/ToddlerTouch.Windows/MainPage.xaml.cs
@@ -25,68 +25,18 @@ namespace DaphnesGame
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random rnd;
         //assume 640x384, a phone in landscape
-
-        int ellipse_radius = 40, drop_border = 2, drop_radius;
-        int ellipse_count = 0; //will scale up based on level
+        int width = 640, height = 384;
+        int ellipse_base_radius = 40, drop_border = 2, drop_radius;
+        int current_ellipse_count = 0; //used to check for level completion
         int level = 1;         //will increase difficulty by increase ellipse_count and adjust sizes/colors
-        //int max_level = 50;    //todo: figure out leveling
-        bool moving = false;   //used to check whether or not an ellipse is being moved
-                               //todo: custom Ellipse object since multi-touch could cause this to become false erroneously
+        int dexterity_help = 10; //within 10px of a drop is close enough. can be adjusted for users with less dexterity.
 
         Ellipse activeEllipse, activeDrop;
-
         List<Ellipse> Ellipses;
         List<Ellipse> Drops;
-
-        private void GenerateLevel(int level)
-        {
-            //level n = n ellipses where n < 7
-            //levels 1-7 only increase ellipses
-            //starting at level 8, adjust colors
-
-            if (level < 8)
-            {
-                //GenerateColorArray() for SolidColorBrush[ColorArray[x]]
-                Color c = Colors.Red;  //debug
-
-                for (int x = 1; x <= level; x++)
-                {
-                    Ellipse ellipse = new Ellipse();
-
-                    ellipse.Fill = new SolidColorBrush(c);
-                    ellipse.Height = ellipse_radius;
-                    ellipse.Width = ellipse_radius;
-                    ellipse.IsTapEnabled = true;
-
-                    ellipse.PointerPressed += Ellipse_PointerPressed;
-                    ellipse.PointerMoved += Ellipse_PointerMoved;
-                    ellipse.PointerReleased += Ellipse_PointerReleased;
-
-                    this.Ellipses.Add(ellipse);
-                    this.canvas.Children.Add(ellipse);
-                    Canvas.SetLeft(ellipse, canvas.Width / 10);
-                    Canvas.SetTop(ellipse, canvas.Height / 10);
-                    Canvas.SetZIndex(ellipse, 1); //always keep ellipses on top of their drops
-
-
-                    Ellipse drop = new Ellipse();
-                    drop.Fill = new SolidColorBrush(Colors.Transparent);
-
-                    drop.Stroke = new SolidColorBrush(c);
-                    drop.StrokeThickness = drop_border;
-                    drop.Height = drop_radius;
-                    drop.Width = drop_radius;
-                    drop.IsTapEnabled = false; //todo: tap provides hint
-
-                    this.Drops.Add(drop);
-                    this.canvas.Children.Add(drop);
-                    Canvas.SetLeft(drop, canvas.Width * .33);
-                    Canvas.SetTop(drop, canvas.Height * .33);
-                    Canvas.SetZIndex(drop, 0); //always keep drops behind their ellipses
-                }
-            }
-        }
+        Color last_color;
 
         public MainPage()
         {
@@ -98,7 +48,8 @@ namespace DaphnesGame
 
         private void GameInit()
         {
-            drop_radius = ellipse_radius + (2 * drop_border);
+            rnd = new Random();
+            drop_radius = ellipse_base_radius + (2 * drop_border);
 
             this.Ellipses = new List<Ellipse>();
             this.Drops = new List<Ellipse>();
@@ -107,7 +58,87 @@ namespace DaphnesGame
         private void GameRun()
         {
             GenerateLevel(level);
+        }
 
+        private Color GetRandomColor()
+        {
+            int a = 255;
+            int r = rnd.Next(0, 256);
+            int g = rnd.Next(0, 256);
+            int b = rnd.Next(0, 256);
+
+            //avoid any grayscale(ish) colors
+            //Grayscale is when R, G & B are all the same. I want to avoid
+            //gray(ish) colors so +/- 10 should do the trick.
+            if (Math.Abs(r - b) < 10 && Math.Abs(r - g) < 10 && Math.Abs(g - b) < 10)
+                return GetRandomColor();
+                
+
+            Color c = Color.FromArgb((byte)a, (byte)r, (byte)g, (byte)b);
+            last_color = c;
+            return c;
+        }
+
+        private void GenerateLevel(int level)
+        {
+            int radius_adjustor = 0; //randomize the size of our ellipses
+
+            //level n = n ellipses where n < 7
+            //levels 1-7 only increase ellipses
+            //starting at level 8, adjust colors
+            if (level < 8)
+            {
+                for (int x = 1; x <= level; x++)
+                {
+                    SolidColorBrush br = new SolidColorBrush(GetRandomColor());
+
+                    if (rnd.Next(0, 4) < 3) //75% chance of only increasing by 20px
+                        radius_adjustor = rnd.Next(20);
+                    else
+                        radius_adjustor = rnd.Next(30, 40);
+
+                    //generate ellipse
+                    Ellipse ellipse = new Ellipse();
+                    ellipse.Fill = br;
+                    ellipse.Height = ellipse_base_radius + radius_adjustor;
+                    ellipse.Width = ellipse_base_radius + radius_adjustor;
+                    ellipse.IsTapEnabled = true;
+                    ellipse.PointerPressed += Ellipse_PointerPressed;
+                    ellipse.PointerMoved += Ellipse_PointerMoved;
+                    ellipse.PointerReleased += Ellipse_PointerReleased;
+                    this.Ellipses.Add(ellipse);
+                    this.canvas.Children.Add(ellipse);
+                    Canvas.SetLeft(ellipse, rnd.Next(width - (int)ellipse.Width));
+                    Canvas.SetTop(ellipse, rnd.Next(height - (int)ellipse.Height));
+                    Canvas.SetZIndex(ellipse, 1); //always keep ellipses on top of their drops
+
+
+                    //generate drop
+                    Ellipse drop = new Ellipse();
+                    drop.Fill = new SolidColorBrush(Colors.Transparent);
+                    drop.Stroke = br;
+                    drop.StrokeThickness = drop_border;
+                    drop.Height = drop_radius + radius_adjustor;
+                    drop.Width = drop_radius + radius_adjustor;
+                    drop.IsTapEnabled = false; //todo: tap provides hint
+                    this.Drops.Add(drop);
+                    this.canvas.Children.Add(drop);
+                    Canvas.SetLeft(drop, rnd.Next(width - (int) drop.Width));
+                    Canvas.SetTop(drop, rnd.Next(height - (int) drop.Height));
+                    Canvas.SetZIndex(drop, 0); //always keep drops behind their ellipses
+
+                    current_ellipse_count++;
+                }
+            }
+        }
+
+        private void LevelComplete()
+        {
+            //todo: animation/sound for winning
+            level++;
+            this.canvas.Children.Clear();
+            GameInit();
+            GameRun();
         }
 
         private void CheckDropEllipse()
@@ -118,17 +149,20 @@ namespace DaphnesGame
             double drTop = Canvas.GetTop(activeDrop);
 
             //auto-grab when "close enough" to the drop
-            if (Math.Abs(elLeft - drLeft) <= 5 && Math.Abs(elTop - drTop) <= 5)
+            if (Math.Abs(elLeft - drLeft) <= 5 && Math.Abs(elTop - drTop) <= dexterity_help)
                 DropEllipse();
         }
-
         private void DropEllipse()
         {
             this.activeDrop.Stroke = new SolidColorBrush(Colors.AliceBlue);
             this.activeDrop.Fill = new SolidColorBrush((this.activeEllipse.Fill as SolidColorBrush).Color);
             this.activeDrop.Stroke = new SolidColorBrush((this.activeEllipse.Fill as SolidColorBrush).Color);
             this.canvas.Children.Remove(activeEllipse);
-            ellipse_count--;
+            current_ellipse_count--;
+
+            //todo: check for level completion
+            if (current_ellipse_count == 0)
+                LevelComplete();
         }
 
         private void Ellipse_PointerMoved(object sender, PointerRoutedEventArgs e)
@@ -138,8 +172,8 @@ namespace DaphnesGame
             if(activeEllipse != null && activeDrop != null)
             {
 
-                //if (moving && el == activeEllipse)
-                if (e.Pointer.IsInContact && el == activeEllipse)
+                if (e.Pointer.IsInContact //is moving/moveable
+                    && el == activeEllipse)
                 {      
                     PointerPoint pointer_pos_in_canvas = e.GetCurrentPoint(el.Parent as Canvas);
 
@@ -150,22 +184,18 @@ namespace DaphnesGame
                 }
             }
         }
-
         private void Ellipse_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             var el = sender as Ellipse;
             el.CapturePointer(e.Pointer);
-            //moving = true;
 
             this.activeEllipse = el;
             this.activeDrop = this.Drops.Find(d => (el.Fill as SolidColorBrush).Color == (d.Stroke as SolidColorBrush).Color);
         }
-
         private void Ellipse_PointerReleased(object sender, PointerRoutedEventArgs e)
         {
             var el = sender as Ellipse;
             el.ReleasePointerCapture(e.Pointer);
-            //moving = false;
             this.activeEllipse = null;
             this.activeDrop = null;
         }
